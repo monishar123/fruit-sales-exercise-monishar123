38b96fadda69f565ce64d116bdbd802714da5a14
Your Name <your.email@example.com>
2024-09-15 12:42:47 -0400

    First Commit

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,10 @@
-# add your code here
+import pandas as pd
+
+# Create the DataFrame
+data = {'Apples': [35, 41], 'Bananas': [21, 34]}
+index = ['2017 Sales', '2018 Sales']
+fruit_sales = pd.DataFrame(data, index=index)
+
+# Write the DataFrame to a CSV file
+fruit_sales.to_csv('fruit.csv')
+
